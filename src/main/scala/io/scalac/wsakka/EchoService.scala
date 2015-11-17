4305dacb48217437b9f90cb4ae103df084a61105
Mariusz Nosiński <marioosh@5dots.pl>
2015-11-17 20:19:52 +0100

    stage2 almost working echo websocket.

---
+++ src/main/scala/io/scalac/wsakka/EchoService.scala
@@ -0,0 +1,16 @@
+package io.scalac.wsakka
+
+import akka.http.scaladsl.model.ws.{TextMessage, Message}
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server._
+import akka.stream.scaladsl.Flow
+
+object EchoService {
+  def route: Route = path("ws-echo") {
+    get {
+      handleWebsocketMessages(echoService)
+    }
+  }
+
+  val echoService: Flow[Message, Message, Any] = ???
+}
